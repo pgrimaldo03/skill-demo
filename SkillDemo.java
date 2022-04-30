0ed9c1a59d510827545f6b411a4eb9e33abe2fa7
pgrimaldo03 <pgrimaldo@ucsd.edu>
2022-04-29 22:47:26 -0700

    Added all files and test case

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,6 +1,7 @@
 
 public class SkillDemo {
 
-
-
+    public static int multiply(int i, int j){
+        return i * j;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void muplication(){
+        assertEquals(6, SkillDemo.multiply(2, 2)); //Failing test
+    }
+}
