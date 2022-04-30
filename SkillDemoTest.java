41853101c128e428c01408641033b08b1d96fae1
pgrimaldo03 <pgrimaldo@ucsd.edu>
2022-04-29 22:51:27 -0700

    passing test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,6 +5,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void muplication(){
-        assertEquals(6, SkillDemo.multiply(2, 2)); //Failing test
+        assertEquals(6, SkillDemo.multiply(2, 3)); //Passing test
     }
 }
